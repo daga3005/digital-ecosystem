d893445b79cc480f698cedb94e121a935f9ed34f
Daniel Alejandro <garcdanielalejandro@gmail.com>
2026-05-02 02:20:40 -0400

    eliminando videos de prueba

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Cop from "./components/home/Cop"
 import Cta from "./components/home/Cta"
 import Introduccion from "./components/home/Introduction";
 import Footer from "./components/home/Footer"
-import { Video1, Video2 } from "./components/home/Videos";
+//import { Video1, Video2 } from "./components/home/Videos";
 
 export default function Home() {
   return (
@@ -14,10 +14,10 @@ export default function Home() {
     <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <HeroApp/>
       <Definition/>
-      <Video1/>
+      
       <Introduccion/>
       <Cursos/>
-      <Video2/>
+      
       <Cop/>
       <Cta/>
       <Footer/>
